47f6b5e9c5b5bd967563b60816b711a93d166a05
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2022-10-17 10:36:28 -0300

    tela detalhes

--- frontend/views/arearestrita/Detalhe.js
+++ frontend/views/arearestrita/Detalhe.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import MenuAreaRestrita from "../../assets/components/MenuAreaRestrita";
 import { css } from "../../assets/css/Css";
 
-export default function Detalhe() {
+export default function Detalhe({ navigation }) {
   return (
-    <View style={css.container}>
+    <View style={[css.container, css.containerTop]}>
+      <MenuAreaRestrita title="Detalhes" navigation={navigation} />
       <Text>Tela detalhes da solicitação</Text>
     </View>
   );
